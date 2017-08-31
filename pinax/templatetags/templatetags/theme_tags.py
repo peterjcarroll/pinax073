ab21ef4208d08d4efe110a4c9baea7101da69c0f
Peter Carroll <peter@eringal.com>
2017-08-30 23:45:37 -0500

    Fixed silk template tag

--- pinax/templatetags/templatetags/theme_tags.py
+++ pinax/templatetags/templatetags/theme_tags.py
@@ -1,8 +1,12 @@
 from django import template
 from django.conf import settings
+from django.utils.html import format_html
 
 register = template.Library()
 
 @register.simple_tag
 def silk(name):
-    return """<img src="%spinax/images/silk/icons/%s.png" />""" % (settings.STATIC_URL, name)
+    return format_html("""<img src="{}pinax/images/silk/icons/{}.png" />""",
+        settings.STATIC_URL,
+        name,
+    )
